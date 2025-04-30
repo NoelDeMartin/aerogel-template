49fc8d5bc3505495b3859c4ed961925ef376ad1e
Noel De Martin <noel@noeldemartin.com>
2025-04-30 19:58:51 +0200

    Configure routing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
     <AppLayout>
         <AppHeader />
-        <main class="mx-auto w-full max-w-screen-md grow flex-col items-center justify-center py-8 text-center">
-            <Welcome />
-        </main>
+        <router-view />
     </AppLayout>
 </template>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,12 @@
 <template>
-    <nav class="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2 pt-8">
-        <h1 class="text-2xl font-semibold tracking-tight">
-            {{ $t('app.title') }}
+    <nav class="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2 px-4 pt-8 md:px-0">
+        <h1>
+            <Link route="home" class="text-2xl font-semibold tracking-tight">
+                {{ $t('app.title') }}
+            </Link>
         </h1>
+        <div class="grow" />
+        <ErrorLogs />
         <Account />
     </nav>
 </template>
