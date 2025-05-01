5266186352214a60ee43031a7640304db2d362c2
Noel De Martin <noel@noeldemartin.com>
2025-05-01 09:43:30 +0200

    Update header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2 px-4 pt-8 md:px-0">
+    <header class="max-w-screen-content mx-auto flex w-full items-center justify-between gap-2 px-4 pt-8 md:px-0">
         <h1>
             <Link route="home" class="text-2xl font-semibold tracking-tight">
                 {{ $t('app.title') }}
