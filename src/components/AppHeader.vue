92f8331aa4695bba30b12ee44dc0b2a1857c908b
Noel De Martin <noel@noeldemartin.com>
2025-05-01 08:16:33 +0200

    Fix header markup

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2 px-4 pt-8 md:px-0">
+    <header class="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2 px-4 pt-8 md:px-0">
         <h1>
             <Link route="home" class="text-2xl font-semibold tracking-tight">
                 {{ $t('app.title') }}
@@ -8,5 +8,5 @@
         <div class="grow" />
         <ErrorLogs />
         <Account />
-    </nav>
+    </header>
 </template>
