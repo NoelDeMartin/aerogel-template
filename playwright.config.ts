015a1dd3aa380f93b89cb9b72fd2d6c370faf729
Noel De Martin <noel@noeldemartin.com>
2026-06-16 18:40:50 +0200

    Tweak playwright CI config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,15 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './e2e',
-    timeout: 30000,
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: process.env.CI ? 'github' : 'list',
+    reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
     use: {
         baseURL: 'http://localhost:5001',
-        trace: 'on-first-retry',
+        trace: 'retain-on-failure',
+        screenshot: 'only-on-failure',
     },
     projects: process.env.CI
         ? [
